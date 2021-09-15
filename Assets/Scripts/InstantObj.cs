b6ba36d6688b84b9ee17094b0027ad1568b02fd9
gomadare <minecrafter1832810@gmail.com>
2021-09-16 06:39:22 +0900

    Cubeを一つ生成

---
+++ Assets/Scripts/InstantObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantObj : MonoBehaviour
+{
+    [SerializeField] private GameObject PrefabCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(PrefabCube);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
