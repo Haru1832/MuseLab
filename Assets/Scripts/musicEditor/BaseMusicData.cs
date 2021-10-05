c42dabb78333eb0f28b08aac043864fda9d39802
gomadare <minecrafter1832810@gmail.com>
2021-10-05 21:39:53 +0900

    musicEditor追加

---
+++ Assets/Scripts/musicEditor/BaseMusicData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseMusicData : MonoBehaviour
+{
+    public List<setValues> _valueses;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+    [System.Serializable]
+    public struct setValues
+    {
+        public GameObject gameObject;
+        public float offset;
+
+        public setValues(GameObject game,float offset)
+        {
+            this.gameObject = game;
+            this.offset = offset;
+        }
+    }
+}
---
+++ Assets/Scripts/musicEditor/testEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace musicEditor
+{
+    public class testEditor : MonoBehaviour
+    {
+        [SerializeField] private BaseMusicData _data;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                _data._valueses.Add(new BaseMusicData.setValues(null,1));
+            }
+        }
+    }
+
+    
+}
