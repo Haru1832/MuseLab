8310be46d8282f4cd86a473641e58aa6fbb194bb
gomadare <minecrafter1832810@gmail.com>
2021-10-07 22:14:14 +0900

    gameシーン

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerShooter))]
+[RequireComponent(typeof(PlayerViewMove))]
+[RequireComponent(typeof(PlayerSEPlayer))]
 public class Player : MonoBehaviour
 {
     public Camera fpsCam;
